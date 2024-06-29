060d7d7f35ab9fbd50ff899b63d13c59bfe38799
Hamza-Naeem-098 <159928737+Hamza-Naeem-098@users.noreply.github.com>
2024-06-29 15:30:55 +0500

    Add files via upload

---
+++ react/reactproject/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <div>
+  <h1>Hello World</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos possimus 
+    debitis labore natus doloribus consequatur quas earum, nisi facilis optio.</p>
+    <img src="/vite.svg" alt="" />
+ </div>
+)
